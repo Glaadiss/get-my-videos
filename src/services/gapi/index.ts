4dd7c5d60f3c46fa32d1bf87ff6b6a0f2c2a0484
Glaadiss <bartekgladys@gmail.com>
2018-07-02 07:29:01 +0200

    Add gapi wrapper.

---
+++ src/services/gapi/index.ts
@@ -0,0 +1,59 @@
+const API_KEY = "AIzaSyCHD4aCb2iHf-8vcx9lbN71RisTtBYxit0";
+const CLIENT_ID =
+  "650362998502-hgepbavbsu927vv11gnm0pt7ohfjpr1g.apps.googleusercontent.com";
+
+import { RootObject } from "./responseTypings";
+import { VideoQuery } from "../actions/videos";
+
+function loadYoutubeApi() {
+  return new Promise((resolve, reject) => {
+    const { gapi } = window as any;
+
+    gapi.client
+      .init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        scope: "https://www.googleapis.com/auth/youtube.force-ssl",
+        discoveryDocs: [
+          "https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"
+        ]
+      })
+      .then(() => {
+        resolve();
+      });
+  });
+}
+
+function getVideos({ q, maxResults = 10 }: VideoQuery) {
+  const { gapi } = window as any;
+  return new Promise((resolve, reject) => {
+    const request = gapi.client.youtube.search.list({
+      q,
+      part: "snippet",
+      maxResults
+    });
+    request.execute((response: RootObject) => {
+      resolve(response);
+    });
+  });
+}
+
+function signIn() {
+  getAuthInstance().signIn();
+}
+
+function signOut() {
+  getAuthInstance().signOut();
+}
+
+function signInListen(cb: () => void) {
+  getAuthInstance().isSignedIn.listen(() => cb());
+}
+
+function getAuthInstance() {
+  const { gapi } = window as any;
+  const GoogleAuth = gapi.auth2.getAuthInstance();
+  return GoogleAuth;
+}
+
+export { loadYoutubeApi, getVideos, signIn, signOut, signInListen };
