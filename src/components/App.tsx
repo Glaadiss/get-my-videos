76578e0e05f96699c77c095f7b040bff742dc811
Glaadiss <bartekgladys@gmail.com>
2018-07-07 20:41:30 +0200

    Add custom MuiTheme to App.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,17 +5,40 @@ import { Provider } from "react-redux";
 import Sidebar from "./Sidebar";
 import { store } from "../services/store";
 import Content from "./Content";
-// import { loadYoutubeApi, getVideos } from "../services/gapi";
+import { loadYoutubeApi } from "../services/gapi";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#757ce8",
+      main: "#3f50b5",
+      dark: "#002884",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#fff"
+    }
+  }
+});
 
 class App extends React.Component {
+  public componentDidMount() {
+    loadYoutubeApi();
+  }
   public render() {
     return (
       <Provider store={store}>
-        <div>
-          <Navbar />
-          <Sidebar />
-          <Content />
-        </div>
+        <MuiThemeProvider theme={theme}>
+          <div>
+            <Navbar />
+            <Sidebar />
+            <Content />
+          </div>
+        </MuiThemeProvider>
       </Provider>
     );
   }
