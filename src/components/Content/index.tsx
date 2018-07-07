b21bfed105d64e0d3bc6d387b6aec0e13b6a082d
Glaadiss <bartekgladys@gmail.com>
2018-07-07 20:45:28 +0200

    Add debouncing and connect to redux Content component

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,24 +1,30 @@
 import * as React from "react";
 import YouTube from "react-youtube";
+import { connect } from "react-redux";
+import { debounce } from "lodash";
+type Props = {
+  open: boolean;
+};
 
 type State = {
   width: string;
   height: string;
 };
 
-class Content extends React.Component<{}, State> {
+class Content extends React.Component<Props, State> {
   public state = {
-    height: `${window.innerHeight - 100}`,
-    width: `${window.innerWidth - 300}`
+    height: `${window.innerHeight - 150}`,
+    width: `100%`
   };
 
+  private resize = debounce((_: any): void => {
+    this.setState({
+      height: `${window.innerHeight - 150}`
+    });
+  }, 500);
+
   public componentDidMount() {
-    window.onresize = (_: any): void => {
-      this.setState({
-        height: `${window.innerHeight - 100}`,
-        width: `${window.innerWidth - 300}`
-      });
-    };
+    window.onresize = this.resize;
   }
 
   public render() {
@@ -26,13 +32,17 @@ class Content extends React.Component<{}, State> {
       height: this.state.height,
       width: this.state.width,
       playerVars: {
-        // https://developers.google.com/youtube/player_parameters
         autoplay: 0
       }
     };
 
     return (
-      <div style={{ paddingLeft: "255px", paddingTop: "64px" }}>
+      <div
+        style={{
+          paddingLeft: this.props.open ? "32%" : "0",
+          paddingTop: "100px"
+        }}
+      >
         <YouTube videoId="2g811Eo7K8U" opts={opts} onReady={this._onReady} />
       </div>
     );
@@ -44,4 +54,17 @@ class Content extends React.Component<{}, State> {
   }
 }
 
-export default Content;
+type StateToProps = {
+  menu: { open: boolean };
+  videos: { videos: any[] };
+};
+const mapStateToProps = (state: StateToProps) => {
+  return { open: state.menu.open && state.videos.videos.length > 0 };
+};
+
+const ConnectedContent = connect(
+  mapStateToProps,
+  null
+)(Content);
+
+export default ConnectedContent;
