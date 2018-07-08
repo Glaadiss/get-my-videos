23e8531bc576be73b73da8afa2354ccc430013b3
Glaadiss <bartekgladys@gmail.com>
2018-07-08 13:48:23 +0200

    Handle ratings in sagas' index.

--- src/services/sagas/index.ts
+++ src/services/sagas/index.ts
@@ -3,8 +3,11 @@ import { getVideos } from "./videos";
 import { getSuggestions } from "./suggestions";
 import { GET_SUGGESTIONS_REQUEST } from "../actions/suggestions";
 import { GET_VIDEOS_REQUEST } from "../actions/videos";
-
+import { GET_OWNER_LIKES_REQUEST, RATE_REQUEST } from "../actions/currentVideo";
+import { rate, getOwnerLikes } from "./currentVideo";
 export default function* rootSaga() {
   yield takeLatest(GET_SUGGESTIONS_REQUEST, getSuggestions);
   yield takeLatest(GET_VIDEOS_REQUEST, getVideos);
+  yield takeLatest(GET_OWNER_LIKES_REQUEST, getOwnerLikes);
+  yield takeLatest(RATE_REQUEST, rate);
 }
