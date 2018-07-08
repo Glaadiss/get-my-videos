44cc4128fe5ba5259b53d104a4cccab8754ea589
Glaadiss <bartekgladys@gmail.com>
2018-07-08 21:15:56 +0200

    Add yt-initial functions to App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,10 +5,17 @@ import { Provider } from "react-redux";
 import Sidebar from "./Sidebar";
 import { store } from "../services/store";
 import Content from "./Content";
-import { loadYoutubeApi } from "../services/gapi";
+import { loadYoutubeApi, signInListen } from "../services/gapi";
 import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import { Actions } from "../services/actions/user";
+import { Actions as videosActions } from "../services/actions/videos";
 
 const theme = createMuiTheme({
+  overrides: {
+    MuiTypography: {
+      colorInherit: "#fff" as any
+    }
+  },
   palette: {
     primary: {
       light: "#757ce8",
@@ -17,19 +24,42 @@ const theme = createMuiTheme({
       contrastText: "#fff"
     },
     secondary: {
-      light: "#ff7961",
-      main: "#f44336",
-      dark: "#ba000d",
-      contrastText: "#fff"
+      light: "#fff",
+      main: "#eee",
+      dark: "#ddd",
+      contrastText: "#3f50b5"
     }
   }
 });
 
-class App extends React.Component {
-  public componentDidMount() {
-    loadYoutubeApi();
+type S = {
+  hasError: boolean;
+};
+
+class App extends React.Component<{}, S> {
+  public state = {
+    hasError: false
+  };
+
+  public async componentDidMount() {
+    try {
+      await LoadYT();
+      getInitialVideos();
+    } catch (error) {
+      console.log(error);
+      this.setState({ hasError: true });
+    }
   }
+
+  public componentDidCatch(err: any, info: any) {
+    console.log(err, info);
+    this.setState({ hasError: true });
+  }
+
   public render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
     return (
       <Provider store={store}>
         <MuiThemeProvider theme={theme}>
@@ -42,6 +72,17 @@ class App extends React.Component {
       </Provider>
     );
   }
+}
+
+async function LoadYT() {
+  await loadYoutubeApi();
+  signInListen((isSigned: boolean) => {
+    store.dispatch(isSigned ? Actions.signIn() : Actions.signOut());
+  });
+}
+
+function getInitialVideos() {
+  store.dispatch(videosActions.getPopularVideos());
 }
 
 export default App;
